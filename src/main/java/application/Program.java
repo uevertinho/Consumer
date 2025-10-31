b9338ffa1317413ce4f034bbb7c4ef2e033adae9
uevertinho <devvertinho@gmail.com>
2025-10-31 09:09:24 -0300

    Expressão lambda declarada

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -5,6 +5,7 @@ import util.PriceUpdate;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Program {
     public static void main(String[] args) {
@@ -14,8 +15,12 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
+        //desse jeito é bom porque a variável pode vir de fora
+        //inclusive uma variável lida por um usuário
+        Consumer<Product> cons = p -> p.setPrice(p.getPrice() * 1.1);
+
         //referência pro metodo que fizemos dentro de Product
-        list.forEach(Product::nonstaticPriceUpdate);
+        list.forEach(cons);
 
         //to usando um reference method para o println
         list.forEach(System.out::println);
